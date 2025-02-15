d999a0ead7d8c7fc35e4c3f2038698d2390756a3
JonathanVarild <jonathan.varild@gmail.com>
2025-02-15 02:10:49 +0100

    Solved The Hungry Birds Problem  (one producer - multiple consumers) & The Bear and Honeybees Problem (multiple producers - one consumer)
    Co-authored-by: Elias Holm <eliasholm8@gmail.com>

--- hw3/honeybees.c
+++ hw3/honeybees.c
@@ -4,6 +4,79 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <semaphore.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define mutex_lock_name "./mutex_lock"
+#define wake_bear_signal_name "./wake_bear_signal"
+
+sem_t *mutex_lock;
+sem_t *wake_bear_signal;
+
+pthread_t bear_thread;
+pthread_t *bee_threads;
+
+int pot_capacity = 7;
+int num_of_bees = 3;
+int current_honey;
+
+void *bee_worker(void *arg)
+{
+    // Get id.
+    int id = (int)arg;
+
+    // Loop indefinitely.
+    while (true)
+    {
+        // Wait for the mutex to unlock.
+        sem_wait(mutex_lock);
+
+        // Add to the honey.
+        current_honey++;
+
+        // PRint to console.
+        printf("Bee #%d added to the pot. Now: %d.\n", id, current_honey);
+
+        // Check if the pot is full and signal for the bear, else unlock and let another bee work.
+        if (current_honey == pot_capacity) {
+            printf("Bee #%d is signaling for the bear!\n", id);
+            sem_post(wake_bear_signal);
+        }
+        else {
+            sem_post(mutex_lock);
+        }
+
+        // Sleep a random amount of seconds.
+        usleep((100 + rand() % 2000) * 1000);
+    }
+
+    pthread_exit(NULL);
+}
+
+void *parent_worker()
+{
+    // Loop indefinitely.
+    while (true)
+    {
+        // Wait for the pot to be full.
+        sem_wait(wake_bear_signal);
+
+        // Reset the pot.
+        current_honey = 0;
+
+        // Print to console.
+        printf("The bear ate the pot. Now %d.\n", current_honey);
+
+        // Unlock the mutex again so the bees can work.
+        sem_post(mutex_lock);
+
+        // Sleep a random amount of seconds.
+        usleep((1000 + rand() % 2000) * 1000);
+    }
+
+    pthread_exit(NULL);
+}
 
 /// @brief The main function of the program.
 /// @param argc The number of arguments.
@@ -11,20 +84,74 @@
 /// @return The exit code of the program.
 int main(int argc, char *argv[])
 {
-    // Check if the file name is provided.
-    if (argc < 2)
+    // First argument is the pot_capacity.
+    if (argc >= 2)
     {
-        printf("Missing arguments. %d\n", argc);
-        exit(1);
+        pot_capacity = atoi(argv[1]);
     }
-    
+
+    // Second argument is num_of_bees.
+    if (argc >= 3)
+    {
+        num_of_bees = atoi(argv[2]);
+    }
+
+    // Print config.
+    printf("Pot capacity is %d and the number of bees are %d.\n", pot_capacity, num_of_bees);
+
+    // Initalize "randomness".
+    srand(time(NULL));
+
+    // Ensure the symaphores doesn't already exist.
+    sem_unlink(mutex_lock_name);
+    sem_unlink(wake_bear_signal_name);
+
+    // Open named symaphores.
+    mutex_lock = sem_open(mutex_lock_name, O_CREAT, 0644, 1);
+    wake_bear_signal = sem_open(wake_bear_signal_name, O_CREAT, 0644, 0);
+
+    // Check so the semaphores were created successfully.
+    if (mutex_lock == (void *)-1 || wake_bear_signal == (void *)-1)
+    {
+        printf("Could not create one or more named semaphores.\n");
+        return 1;
+    }
+
+    // Set current_honey.
+    current_honey = 0;
+
     // Setup thread attributes.
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
 
+    // Allocate an array for the number of bees.
+    bee_threads = malloc(num_of_bees * sizeof(pthread_t));
 
+    // Create the bee threads.
+    for (int i = 0; i < num_of_bees; i++)
+    {
+        pthread_create(&bee_threads[i], &attr, bee_worker, (void *)i);
+    }
 
+    // Create a thread for the bear.
+    pthread_create(&bear_thread, &attr, parent_worker, NULL);
+
+    // Wait for the bee threads to finish.
+    for (int i = 0; i < num_of_bees; i++)
+    {
+        pthread_join(bee_threads[i], NULL);
+    }
+
+    // Wait for the bear thread to finish.
+    pthread_join(bear_thread, NULL);
+
+    // Close the semaphores.
+    sem_close(mutex_lock);
+    sem_close(wake_bear_signal);
+
+    // Free memory.
+    free(bee_threads);
 
     return 0;
 }
--- hw3/hungrybirds.c
+++ hw3/hungrybirds.c
@@ -4,6 +4,91 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <semaphore.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define mutex_lock_name "./mutex_lock"
+#define refill_signal_name "./refill_signal"
+#define worms_available_name "./worms_available"
+
+sem_t *mutex_lock;
+sem_t *refill_signal;
+sem_t *worms_available;
+
+pthread_t parent_bird;
+pthread_t *baby_birds;
+
+int worms_to_add = 7;
+int num_of_birds = 3;
+int worm_count;
+
+void *baby_worker(void *arg)
+{
+    // Get id.
+    int id = (int)arg;
+
+    // Loop indefinitely.
+    while (true)
+    {
+        // Wait for food and decrement the food count.
+        sem_wait(worms_available);
+
+        // Lock the mutex by setting it back to 0.
+        sem_wait(mutex_lock);
+
+        // Decrement the worm count.
+        printf("Bird #%d ate a worm. There are %d worms left.\n", id, --worm_count);
+
+        // Check if we need to ask for more worms.
+        if (worm_count == 0)
+        {
+            printf("WE NEED MORE FOOD!\n");
+            sem_post(refill_signal);
+        }
+
+        // Unlock the mutex.
+        sem_post(mutex_lock);
+
+        // Sleep a random amount of seconds.
+        usleep((100 + rand() % 2000) * 1000);
+    }
+
+    pthread_exit(NULL);
+}
+
+void *parent_worker()
+{
+    // Loop indefinitely.
+    while (true)
+    {
+        // Wait for a signal that we need to refill.
+        sem_wait(refill_signal);
+
+        // Sleep a random amount of seconds.
+        usleep((1000 + rand() % 2000) * 1000);
+
+        // Lock the mutex by setting it back to 0.
+        sem_wait(mutex_lock);
+
+        // Refill the worms.
+        for (int i = 0; i < worms_to_add; i++)
+        {
+            sem_post(worms_available);
+        }
+
+        // Update the integer counter.
+        worm_count += worms_to_add;
+
+        // Unlock the mutex by setting it back to 1.
+        sem_post(mutex_lock);
+
+        // Print to console.
+        printf("[Parent] Added %d worms to the count. Now: %d\n", worms_to_add, worm_count);
+    }
+
+    pthread_exit(NULL);
+}
 
 /// @brief The main function of the program.
 /// @param argc The number of arguments.
@@ -11,20 +96,74 @@
 /// @return The exit code of the program.
 int main(int argc, char *argv[])
 {
-    // Check if the file name is provided.
-    if (argc < 2)
+    // First argument is the worms_to_add.
+    if (argc >= 2)
     {
-        printf("Missing arguments. %d\n", argc);
-        exit(1);
+        worms_to_add = atoi(argv[1]);
     }
-    
+
+    // Second argument is num_of_birds.
+    if (argc >= 3)
+    {
+        num_of_birds = atoi(argv[2]);
+    }
+
+    // Initalize "randomness".
+    srand(time(NULL));
+
+    // Ensure the symaphores doesn't already exist.
+    sem_unlink(mutex_lock_name);
+    sem_unlink(refill_signal_name);
+    sem_unlink(worms_available_name);
+
+    // Open named symaphores.
+    mutex_lock = sem_open(mutex_lock_name, O_CREAT, 0644, 1);
+    refill_signal = sem_open(refill_signal_name, O_CREAT, 0644, 0);
+    worms_available = sem_open(worms_available_name, O_CREAT, 0644, worms_to_add);
+
+    // Check so the semaphores were created successfully.
+    if (mutex_lock == (void *)-1 || refill_signal == (void *)-1 || worms_available == (void *)-1)
+    {
+        printf("Could not create one or more named semaphores.\n");
+        return 1;
+    }
+
+    // Set worm_count.
+    worm_count = worms_to_add;
+
     // Setup thread attributes.
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
 
+    // Allocate an array for the number of baby birds.
+    baby_birds = malloc(num_of_birds * sizeof(pthread_t));
 
+    // Create the baby bird threads.
+    for (int i = 0; i < num_of_birds; i++)
+    {
+        pthread_create(&baby_birds[i], &attr, baby_worker, (void *)i);
+    }
 
+    // Create a thread for the parent bird.
+    pthread_create(&parent_bird, &attr, parent_worker, NULL);
+
+    // Wait for the baby bird threads to finish.
+    for (int i = 0; i < num_of_birds; i++)
+    {
+        pthread_join(baby_birds[i], NULL);
+    }
+
+    // Wait for the parent bird thread to finish.
+    pthread_join(parent_bird, NULL);
+
+    // Close the semaphores.
+    sem_close(mutex_lock);
+    sem_close(refill_signal);
+    sem_close(worms_available);
+
+    // Free memory.
+    free(baby_birds);
 
     return 0;
 }
